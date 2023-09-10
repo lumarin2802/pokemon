d5f9e9ef325fc10e27ce97d9da98dc50e1b1d986
lumarin2802 <marinperezluciana@gmail.com>
2023-09-10 10:29:09 -0300

    comenzando ejercicio pokemon

---
+++ src/ejercicioIntegradorPokemon/Pokemon.java
@@ -0,0 +1,77 @@
+package ejercicioIntegradorPokemon;
+
+public abstract class  Pokemon {
+    protected int num_pokedex;
+    protected String nombrePokemon;
+
+    protected  double pesoPokemon;
+
+    protected  String sexo;
+
+    protected int temporadaQueAParece;
+
+    protected  String tipo;
+
+
+
+    protected abstract void atacarPlacaje();
+
+    public int getNum_pokedex() {
+        return num_pokedex;
+    }
+
+    public void setNum_pokedex(int num_pokedex) {
+        this.num_pokedex = num_pokedex;
+    }
+
+    public String getNombrePokemon() {
+        return nombrePokemon;
+    }
+
+    public void setNombrePokemon(String nombrePokemon) {
+        this.nombrePokemon = nombrePokemon;
+    }
+
+    public double getPesoPokemon() {
+        return pesoPokemon;
+    }
+
+    public void setPesoPokemon(double pesoPokemon) {
+        this.pesoPokemon = pesoPokemon;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getTemporadaQueAParece() {
+        return temporadaQueAParece;
+    }
+
+    public void setTemporadaQueAParece(int temporadaQueAParece) {
+        this.temporadaQueAParece = temporadaQueAParece;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Pokemon(int num_pokedex, String nombrePokemon, double pesoPokemon, String sexo, int temporadaQueAParece, String tipo) {
+        this.num_pokedex = num_pokedex;
+        this.nombrePokemon = nombrePokemon;
+        this.pesoPokemon = pesoPokemon;
+        this.sexo = sexo;
+        this.temporadaQueAParece = temporadaQueAParece;
+        this.tipo = tipo;
+
+
+    }
+}
